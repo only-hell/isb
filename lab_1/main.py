d6d63ed61eac6cf5e7974301cfce16f5a9918e81
only-hell <dnslodygin20032005@gmail.com>
2025-03-16 15:31:51 +0400

    первая сборка кода

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,1 +1,13 @@
-print("test text")
+def xor(text, key):
+    result = []
+    for i in range(len(text)):
+        result.append(chr(ord(text[i]) ^ ord(key[i % len(key)])))
+    return ''.join(result)  # Соединяем результат в строку
+
+def main():
+    input_text = ("КАКОЙТО НОРМАЛЬНЫЙ ТЕКСТ")
+    key = "КЛЮЧ"
+    print(xor(input_text, key))
+
+if __name__ == "__main__":
+    main()
