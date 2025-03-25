3cbec1edded5b339d67edacec8b8992f8f7a251f
only-hell <dnslodygin20032005@gmail.com>
2025-03-25 21:05:24 +0400

    задание 2: добавил отдельный файл который хранит названия файлов для сохранения и вынес туда текст для расшифровки, вынес главные части программы в отдельные функции

--- lab_1/task2/main.py
+++ lab_1/task2/main.py
@@ -1,29 +1,27 @@
-with open("cod7.txt", encoding="utf-8") as file:
-    text = file.read()
+import config
 
-# Словарь замен символов на буквы русского алфавита
-# Для удобства работы я заменил символы "/" и "\" на "{" и "}" соответственно
-replacements = {
-    "": "Я", "1": "Й", "d": "Ш", "9": "Г", "p": "Ф", "c": "Щ", "o": "У",
-    "x": "Э", "t": "Х", "}": "Л", "{": "К", "0": "Е", "s": "Ь",
-    "f": "Ц", "7": "Б", "n": "С", "q": "М", "4": "Ж", "z": "Ю",
-    "5": "Д", "r": "П", "w": "Н", "8": "В", "3": "З", ";": "Ы",
-    "/": "К", "6": "А", "m": "Р", "2": "И", "i": "Т", "k": "Ч",
-    "e": "О", "-": " "
-}
 
-# Дешифруем текст
-decoded_text = text
-for old, new in replacements.items():
-    decoded_text = decoded_text.replace(old, new)
+def decode_text(file_path, replacements):
+    # функия расшифровки текста
+    with open(file_path, encoding="utf-8") as file:
+        text = file.read()
 
-# Сохраняем дешифрованный текст
-with open("decoded_text.txt", "w", encoding="utf-8") as file:
-    file.write(decoded_text)
+    for old, new in replacements.items():
+        text = text.replace(old, new)
+    return text
 
-# Сохраняем найденный ключ шифрования
-with open("decryption_key.txt", "w", encoding="utf-8") as file:
-    for key, value in replacements.items():
-        file.write(f"{key} -> {value}\n")
 
-print("Декодированный текст: \n\n", decoded_text)
+def main():
+    decoded_text = decode_text(config.ENCODED_FILE, config.REPLACEMENTS)
+
+    # запись результатов в файлы
+    with open(config.DECODED_FILE, "w", encoding="utf-8") as file:
+        file.write(decoded_text)
+    with open(config.DECRYPTION_KEY_FILE, "w", encoding="utf-8") as file:
+        file.writelines(f"{k} -> {v}\n" for k, v in config.REPLACEMENTS.items())
+
+    print("Декодированный текст:\n\n", decoded_text)
+
+
+if __name__ == "__main__":
+    main()
