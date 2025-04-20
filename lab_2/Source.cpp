5e154c753107ffcea4d0f4d79de6c6de09a43586
only-hell <91896993+only-hell@users.noreply.github.com>
2025-04-20 15:02:25 +0400

    commit 1
    Создал программу для генерации случайной 128 битной последовательности через mt19937

---
+++ lab_2/Source.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <fstream>
+#include <random>
+#include <locale>
+
+int main() {
+    std::setlocale(LC_ALL, "");
+
+    const int sequence_length = 128;  
+    const std::string output_path = "C:/Users/user/Desktop/oib lab2/pythonProject/lab2/sequence.txt";  
+
+    // Открытие файла для записи
+    std::ofstream output_file(output_path);
+    if (!output_file) {
+        std::cerr << "Не удалось открыть файл для записи: " << output_path << std::endl;
+        return 1;
+    }
+
+    // Создание генератора случайных чисел и распределения
+    std::random_device rd;  
+    std::mt19937 rng(rd());  
+    std::uniform_int_distribution<int> bit_dist(0, 1); 
+
+    // Генерация последовательности случайных битов
+    for (int i = 0; i < sequence_length; ++i) {
+        output_file << bit_dist(rng);  
+    }
+
+    
+    output_file.close();
+    std::cout << "Бинарная последовательность длиной 128 бит успешно записана в файл." << std::endl;
+    return 0;
+}
